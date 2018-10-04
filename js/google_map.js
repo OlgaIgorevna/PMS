9f1a9d73451a84734539aa131ffc105955941a47
Olga <olga.pron.kld@gmail.com>
2018-10-04 15:46:47 +0200

    правки

---
+++ js/google_map.js
@@ -0,0 +1,209 @@
+function initMap() {
+	var coordinates = {
+		lat: 54.717727,
+		lng: 20.497477
+	};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: coordinates,
+		zoom: 16,
+		styles: 
+			
+			[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+			
+			
+		
+	});
+
+
+
+
+	var marker = new google.maps.Marker({
+		position: coordinates,
+		map: map
+	});
+
+
+
+
+	//	
+	//		var coordinates = {
+	//		lat: 54.689927,
+	//		lng: 20.464610
+	//	};
+	//	var popupContent = '<p class="content" style="color: #bf2014; font-weight: 700;">улица Суворова, 54у</p>';
+	//	var map = new google.maps.Map(document.getElementById('map'), {
+	//		zoom: 17,
+	//		center: coordinates
+	//	});
+	//	var marker = new google.maps.Marker({
+	//		position: coordinates,
+	//		map: map
+	//	});
+	//
+	//	var infowindow = new google.maps.InfoWindow({
+	//		content: popupContent
+	//	});
+	//	infowindow.open(map, marker);
+	//	
+	//	map.panBy(-200, 0);
+}
