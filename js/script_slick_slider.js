d3c1aa9e6dc56b71c19f19fb83dd4de641d5d35a
Olga <olga.pron.kld@gmail.com>
2018-10-04 12:01:37 +0200

    главная страница

--- js/script_slick_slider.js
+++ js/script_slick_slider.js
@@ -2,48 +2,7 @@ $(window).on("load", function () {
 	var count = 0;
 	var sl_elem = "";
 
-	$(".slider-photo .wrap-item").on("click", function (e) {
 
-		//		console.log("target" + e.target);
-
-		count++;
-
-		$("body").wrapInner('<div class="wrap-body-blur"></div>');
-		$('<div class="antyclick"></div>').appendTo($("body"));
-
-		if (count == 1) {
-			sl_elem = document.getElementsByClassName("slider-modal")[0].outerHTML;
-		}
-
-		$(sl_elem).appendTo($("body"));
-
-		$(".slider-modal").addClass("show-important");
-
-		var index = +$(this).attr("data-slick-index");
-
-
-		$('.slider-modal').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: true,
-			fade: true,
-			adaptiveHeight: true,
-			initialSlide: index
-		});
-
-		function closeBigSlider() {
-			$(".slider-modal.slider-for").slick("unslick");
-			$(".slider-modal.slider-for").remove();
-			$(".antyclick").remove();
-			$(".wrap-body-blur").children().eq(0).unwrap(".wrap-body-blur");
-
-		}
-
-		$(".antyclick").on("click", function (e) {
-			closeBigSlider();
-		});
-
-	});
 
 
 	$('.slider-photo').slick({
@@ -100,7 +59,26 @@ $(window).on("load", function () {
 		autoplay: false,
 		autoplaySpeed: 4000,
 		slidesToShow: 1,
-		slidesToScroll: 1
+		slidesToScroll: 1,
+		responsive: [
+
+		
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+						},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+						}
+	
+						]
 	});
 
 
